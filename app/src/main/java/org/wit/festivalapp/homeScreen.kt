b387679c6ff1026a608e90485ddb0a85b9ffe9c2
davinfortune <davinfortune@gmail.com>
2020-03-09 14:46:31 +0000

    First Commit

---
+++ app/src/main/java/org/wit/festivalapp/homeScreen.kt
@@ -0,0 +1,12 @@
+package org.wit.festivalapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class homeScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
